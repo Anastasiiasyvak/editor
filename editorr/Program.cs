3950878c575ef1d9bbae7191aa941e971936c31a
Anastasiiasyvak <asyvak@kse.org.ua>
2023-04-29 21:28:38 +0300

    file + preparation part

--- editorr/Program.cs
+++ editorr/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Write a sentence:");
+string our_sentence = Console.ReadLine();
+string pathToFile = "/home/nastia/for_new_projects/editorr/words_list.txt";
+string[] separators = {" ", ".", ",", "!", "?", ":", ";"};
+string[] words = our_sentence.Split(separators, StringSplitOptions.RemoveEmptyEntries); // якщо у нас пробіл не один або кома не одна, то він нам видалить ці пробіли і коми, а не зарахує як дод. елемент
+string[] words_in_file = File.ReadAllLines(pathToFile);
+foreach (string word in words)
+{
+    bool found = false;
+    foreach (string word_f in words_in_file)
+    {
+        if (word_f.ToLower() == word.ToLower())
+        {
+            found = true;
+            break;
+        }
+    }
 
-Console.WriteLine("Hello, World!");
+    if (!found)
+    {
+        Console.WriteLine($"Looks like you have a typos in next words:{word} ");
+    }
+}
