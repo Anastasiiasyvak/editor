e3400e004d693c107f9ea31a641d442c5a1c0c43
justmiroslav <mirosmale@gmail.com>
2023-05-05 10:26:54 +0300

    changes in checking words

--- editorr/Program.cs
+++ editorr/Program.cs
@@ -1,9 +1,10 @@
 ﻿Console.WriteLine("Write a sentence:");
 string our_sentence = Console.ReadLine();
-string pathToFile = "/home/nastia/for_new_projects/editorr/words_list.txt";
+string pathToFile = @"C:\Users\Admin\RiderProjects\editor\words_list.txt";
 string[] separators = {" ", ".", ",", "!", "?", ":", ";"};
-string[] words = our_sentence.Split(separators, StringSplitOptions.RemoveEmptyEntries); // якщо у нас пробіл не один або кома не одна, то він нам видалить ці пробіли і коми, а не зарахує як дод. елемент
+string[] words = our_sentence.Split(separators, StringSplitOptions.RemoveEmptyEntries);
 string[] words_in_file = File.ReadAllLines(pathToFile);
+List<string> incorrectWords = new List<string>();
 foreach (string word in words)
 {
     bool found = false;
@@ -16,8 +17,11 @@ foreach (string word in words)
         }
     }
 
-    if (!found)
-    {
-        Console.WriteLine($"Looks like you have a typos in next words:{word} ");
+    if (!found){
+        incorrectWords.Add(word);
     }
+}
+
+if (incorrectWords.Count > 0){
+    Console.WriteLine($"Looks like you have a typos in next words: {string.Join(", ", incorrectWords)}.");
 }
